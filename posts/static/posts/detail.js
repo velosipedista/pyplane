bf871ea1cbd097fe867ff59fa229ca6ebedc2fc6
ganz <ganz@thebeast>
2021-12-10 07:22:11 +0200

    10.12.2021

--- posts/static/posts/detail.js
+++ posts/static/posts/detail.js
@@ -2,8 +2,18 @@ const postBox = document.getElementById('post-box');
 const backBtn = document.getElementById('back-btn');
 const updateBtn = document.getElementById('update-btn');
 const delBtn = document.getElementById('del-btn');
-const url = window.location.href +"data/"
+
+const url = window.location.href + "data/"
+const updateUrl = window.location.href + "update/"
+const deleteUrl = window.location.href + "delete/"
+
+const updateForm = document.getElementById('update-form');
+const deleteForm = document.getElementById('delete-form');
+
 const spinnerBox = document.getElementById("spinner-box");
+
+const titleInput = document.getElementById('id_title');
+const bodyInput = document.getElementById('id_body');
 
 backBtn.addEventListener('click', () => {
     history.back()
@@ -22,15 +32,27 @@ $.ajax({
         }
         let titleEl = document.createElement('h3');
         titleEl.setAttribute('class', 'mt-3 ');
+        titleEl.setAttribute('id', 'title');
+
         let bodyEl = document.createElement('p');
         bodyEl.setAttribute('class', 'mt-1');
+        bodyEl.setAttribute('id', 'body');
+
         titleEl.textContent = data.title;
         bodyEl.textContent = data.body;
         postBox.appendChild(titleEl);
         postBox.appendChild(bodyEl);
+        titleInput.value = data.title;
+        bodyInput.value = data.body;
         spinnerBox.classList.add('not-visible');
     },
     error: function(error){
         console.log(error);
     },
+})
+
+updateForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const title = document.getElementById('title');
+    const body = document.getElementById('body');
 })
--- posts/urls.py
+++ posts/urls.py
@@ -6,6 +6,8 @@ from .views import (
     like_unlike_post,
     post_detail,
     post_detail_data_view,
+    update_post,
+    delete_post,
 )
 
 app_name = 'posts'
@@ -15,5 +17,8 @@ urlpatterns = [
     path('like-unlike/', like_unlike_post, name="like-unlike"),
     path('<pk>/', post_detail, name="post-detail"),
     path('data/<int:num_posts>', load_post_data_view, name="post-data"),
-    path('<pk>/data/', post_detail_data_view, name='post-detail-data')
+    path('<pk>/data/', post_detail_data_view, name='post-detail-data'),
+    path('<pk>/update', update_post, name="post-update"),
+    path('<pk>/delete', delete_post, name="post-delete"),
+
 ]
